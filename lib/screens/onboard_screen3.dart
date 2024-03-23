699561197bfe5fae4d2b55d628ff0c0100a9b869
ZikariaTaj <115821772+ZikariaTaj@users.noreply.github.com>
2024-03-24 00:18:41 +0500

    add alert dialogue onBoarding screen 3

--- lib/screens/onboard_screen3.dart
+++ lib/screens/onboard_screen3.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:lifefit/screens/onboardscreen_1.dart';
 
@@ -34,10 +36,12 @@ class _onBoardScreen3State extends State<onBoardScreen3> {
                 child: TopRow(
                   text: "Login",
                   textcolor: Colors.white,
-                  backgroundcolor: Color(0xFF19b597),
+                  backgroundcolor: const Color(0xFF19b597),
                   onpressed: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginScreen()));
                   },
                 ),
               ),
@@ -55,8 +59,7 @@ class _onBoardScreen3State extends State<onBoardScreen3> {
                 text2:
                     "Let’s start this journey and live healty life together ",
                 onpressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                  showCustomDialog(context);
                 },
               ),
             ),
@@ -65,4 +68,95 @@ class _onBoardScreen3State extends State<onBoardScreen3> {
       ),
     );
   }
+}
+
+void showCustomDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (_) => BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: 5.0,
+        sigmaY: 5.0,
+      ),
+
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            AlertDialog(
+              insetPadding: EdgeInsets.zero,
+              contentPadding: EdgeInsets.zero,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25.0),
+                ),
+              ),
+              content: Builder(
+                builder: (context) {
+                  var height = MediaQuery.of(context).size.height;
+                  var width = MediaQuery.of(context).size.width;
+
+                  // Adjustments to ensure non-negative dimensions
+                  double containerHeight = (height - 400).clamp(0, double.infinity);
+                  double containerWidth = (width - 50).clamp(0, double.infinity);
+
+                  return Container(
+                    width: containerWidth,
+                    child: const Column(
+                      children: [
+                        ListTile(
+                          title: Text("Continue with Email"),
+                          trailing: Icon(Icons.email),
+                        ),
+                        Divider(
+                          indent: 30,
+                          endIndent: 30,
+                        ),
+                        ListTile(
+                          title: Text("Continue with Google"),
+                          trailing: Icon(Icons.apple),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            AlertDialog(
+              insetPadding: EdgeInsets.zero,
+              contentPadding: EdgeInsets.zero,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25.0),
+                ),
+              ),
+              content: Builder(
+                builder: (context) {
+                  var height = MediaQuery.of(context).size.height;
+                  var width = MediaQuery.of(context).size.width;
+
+                  // Adjustments to ensure non-negative dimensions
+                  double containerHeight = (height - 400).clamp(0, double.infinity);
+                  double containerWidth = (width - 50).clamp(0, double.infinity);
+
+                  return Container(
+                    width: containerWidth,
+                    child: const ListTile(
+                      title: Text("Cancel"),
+                      trailing: Icon(Icons.cancel_outlined),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    ),
+  );
 }
